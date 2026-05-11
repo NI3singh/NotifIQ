eb7d45f4bc4bd6f3abc7659e8767004e133a8dd3
Nitin Singh <itsnits333@gmail.com>
2026-05-11 13:53:37 +0530

    Clean up AGP warnings before debug install

--- core/core-database/build.gradle.kts
+++ core/core-database/build.gradle.kts
@@ -1,10 +1,9 @@
 plugins {
     alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.hilt)
     alias(libs.plugins.ksp)
     alias(libs.plugins.kotlin.serialization)
-    id("androidx.room")
+    alias(libs.plugins.androidx.room)
 }
 
 android {
@@ -20,11 +19,9 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
+    room {
+        schemaDirectory("$projectDir/schemas")
     }
-
-
 }
 
 dependencies {
